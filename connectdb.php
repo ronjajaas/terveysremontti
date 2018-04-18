ea6bc2689c6cbb25f39e7a157b91ef47bda9cc12
Ronja Kyrölä <36955440+ronjaky@users.noreply.github.com>
2018-04-18 13:41:39 +0300

    Update connectdb.php

--- connectdb.php
+++ connectdb.php
@@ -1,10 +1,8 @@
 <?php
-$connection = mysqli_connect('localhost', 'root', 'Laurea2018');
-if (!$connection) {
-	die ("Database connection failed") . mysqli_error($connection));
-}
-$select_db = mysqli_select_db($connection, 'terveysremontti');
-if (!$select_db) {
-	die("Database selection failed") . mysqli_error($connection));	
-}
+/* Database connection settings */
+$host = 'localhost';
+$user = 'root';
+$pass = 'Laurea2018';
+$db = 'terveysremontti';
+$mysqli = new mysqli($host,$user,$pass,$db) or die($mysqli->error);
 ?>
