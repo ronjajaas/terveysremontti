db0f62caa05c1b2c020eb7dc3f99e71c2891cfc4
sohvaliini <36955340+sohvaliini@users.noreply.github.com>
2018-04-25 20:28:59 +0300

    Update connectdb.php

--- connectdb.php
+++ connectdb.php
@@ -2,7 +2,7 @@
 /* Database connection settings */
 $host = '127.0.0.1:49207';
 $user = 'azure';
-$pass = '6#vWHD_$';
+$pass = '*******';
 $db = 'terveysremontti';
 $mysqli = new mysqli($host,$user,$pass,$db) or die($mysqli->error);
 ?>
